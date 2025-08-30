0ad3b5f3ae1f153048ae6fd29861420707a163bd
Anushka5678661 <negianushka332@gmail.com>
2025-08-30 22:14:41 +0530

    Day 10(Q1) of 100 day challenge

---
+++ t19.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three sides of the triangle: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // First, check if it's a valid triangle (Triangle inequality rule)
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        if (a == b && b == c) {
+            printf("Equilateral\n");
+        }
+        else if (a == b || b == c || a == c) {
+            printf("Isosceles\n");
+        }
+        else {
+            printf("Scalene\n");
+        }
+    }
+    else {
+        printf("Not a valid triangle\n");
+    }
+
+    return 0;
+}
