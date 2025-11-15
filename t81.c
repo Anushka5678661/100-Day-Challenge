cbecc70ca19ab3a891c5a57bc9e4debcf9015c10
Anushka5678661 <negianushka332@gmail.com>
2025-11-15 18:02:36 +0530

    Day 41(Q1)of 100 day challenge

---
+++ t81.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main() 
+{
+    char str[1000];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); 
+    int count = 0;
+    while (str[count] != '\0' && str[count] != '\n') {
+        count++;
+    }
+    printf("%d\n", count);
+
+    return 0;
+}
