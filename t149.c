d9261915539126fe1433ac5c8e1dd6b01598cdc4
Anushka5678661 <negianushka332@gmail.com>
2025-11-29 22:37:54 +0530

    Day 99 of 100 day challenge

---
+++ t149.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student *s;
+
+    s = (struct Student*) malloc(sizeof(struct Student));
+
+    printf("Enter Name: ");
+    scanf("%s", s->name);
+
+    printf("Enter Roll: ");
+    scanf("%d", &s->roll);
+
+    printf("Enter Marks: ");
+    scanf("%d", &s->marks);
+
+    printf("Name: %s | Roll: %d | Marks: %d\n", s->name, s->roll, s->marks);
+
+    free(s);
+
+    return 0;
+}
