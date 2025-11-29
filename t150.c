6f50891a568bc5f201cfa2da74228b29da03c913
Anushka5678661 <negianushka332@gmail.com>
2025-11-29 22:40:12 +0530

    Day 100 of 100 day challenge

---
+++ t150.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student s;
+    struct Student *ptr = &s;
+
+    printf("Enter Name: ");
+    scanf("%s", ptr->name);
+
+    printf("Enter Roll: ");
+    scanf("%d", &ptr->roll);
+
+    printf("Enter Marks: ");
+    scanf("%d", &ptr->marks);
+
+    printf("Modified Data: Name: %s | Roll: %d | Marks: %d\n",
+           ptr->name, ptr->roll, ptr->marks);
+
+    return 0;
+}
