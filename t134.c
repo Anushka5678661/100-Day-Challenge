75de03a685ca85bbe308c6eb3473b7d3eab317be
Anushka5678661 <negianushka332@gmail.com>
2025-11-29 21:27:35 +0530

    Day 84 of 100 day challenge

---
+++ t134.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    enum Status {SUCCESS, FAILURE, TIMEOUT};
+    char input[10];
+    printf("Enter status (SUCCESS, FAILURE, TIMEOUT): ");
+    scanf("%s", input);
+
+    enum Status s;
+    if(strcmp(input, "SUCCESS") == 0)
+        s = SUCCESS;
+    else if(strcmp(input, "FAILURE") == 0)
+        s = FAILURE;
+    else if(strcmp(input, "TIMEOUT") == 0)
+        s = TIMEOUT;
+    else {
+        printf("Invalid input");
+        return 1;
+    }
+
+    switch(s) {
+        case SUCCESS: printf("Operation successful\n"); break;
+        case FAILURE: printf("Operation failed\n"); break;
+        case TIMEOUT: printf("Operation timed out\n"); break;
+    }
+
+    return 0;
+}
